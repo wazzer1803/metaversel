74a6aa0fb9417a8ca5403cd69a2b4c1b1d2afa18
Sahil Chauksey <113386193+wazzer1803@users.noreply.github.com>
2024-03-04 20:22:03 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => (
     <div className="footer-gradient" />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-black">
         Enter the Kopite Madness
         </h4>
         <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
@@ -25,7 +25,7 @@ const Footer = () => (
             alt="headset"
             className="w-[24px] h-[24px] object-contain"
           />
-          <span className="font-normal text-[16px] text-white">
+          <span className="font-normal text-[16px] text-black">
           Enter  Kopite Madness
           </span>
         </button>
@@ -35,10 +35,10 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">
+          <h4 className="font-extrabold text-[24px] text-black">
             METAVERUS
           </h4>
-          <p className="font-normal text-[14px] text-white opacity-50">
+          <p className="font-normal text-[14px] text-black opacity-50">
             Copyright © 2021 - 2022 Metaversus. All rights reserved.
           </p>
 
