430d3aaf5e01566d4fe2ac6ae9e1d8c0df7850e3
Sahil Chauksey <113386193+wazzer1803@users.noreply.github.com>
2024-03-04 20:57:55 +0530

    Update Feedback.jsx

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -77,12 +77,12 @@ const Feedback = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
     >
-{/*       <motion.div
+      <motion.div
         variants={fadeIn('right', 'tween', 0.2, 1)}
         className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
       >
         <div className="feedback-gradient" />
-{/*         <div>
+        <div>
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
             Samantha
           </h4>
@@ -95,11 +95,10 @@ const Feedback = () => (
           “With the development of today's technology, metaverse is very
           useful for today's work, or can be called web 3.0. by using
           metaverse you can use it as anything”
-        </p> */}
-{/*         yes
-      </motion.div> */} */}
-    <p className="text-black font-bold"
-      >Yess</p>
+        </p>
+        <p className="text-black font-bold"> YOLO</p>
+      </motion.div>
+    
 
       <motion.div
         variants={fadeIn('left', 'tween', 0.2, 1)}
