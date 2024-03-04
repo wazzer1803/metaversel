065b0c717ee1b8b244794ddabe210cd3182c7db7
Sahil Chauksey <113386193+wazzer1803@users.noreply.github.com>
2024-03-04 19:14:39 +0530

    Update CustomTexts.jsx

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -6,7 +6,7 @@ import { textContainer, textVariant2} from '../utils/motion';
 
 
 export const TypingText = ({title,textStyles}) => (
-  <motion.p variants={textContainer} className={`font-normal text-[14px] text-secondary-white ${textStyles}`}>
+  <motion.p variants={textContainer} className={`font-normal text-[14px] text-black ${textStyles}`}>
 {
 Array.from(title).map((letter,index)=>{
  return(<motion.span variants={textVariant2} key={index}>
@@ -17,7 +17,7 @@ Array.from(title).map((letter,index)=>{
 );
 
 export const TitleText = ({title,textStyles}) => (
-  <motion.h2 variants={textVariant2} initial="hidden" whileInView="show" className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}>
+  <motion.h2 variants={textVariant2} initial="hidden" whileInView="show" className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-black ${textStyles}`}>
 {title}
   </motion.h2>
 );
