327b26766d8340eab82a1cec66b6e8d147781d3b
Sahil Chauksey <113386193+wazzer1803@users.noreply.github.com>
2024-03-04 20:30:09 +0530

    Update Feedback.jsx

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -20,15 +20,15 @@ const Feedback = () => (
       >
         <div className="feedback-gradient" />
         <div>
-          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-black">
           Jürgen  Klopp
           </h4>
-          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-black">
            The Gaffer
           </p>
         </div>
 
-        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-black">
           “We created our own story and history and didn't try to do what our wonderful dads and grandfathers did with this club.
           The challenge is to stay cool enough to handle the pressure at the moment to succeed in the future.To be successful, you need results in a row. You can’t win, lose, win, lose.”
         </p>
