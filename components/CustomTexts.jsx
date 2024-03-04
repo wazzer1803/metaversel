daa6719c61e597700e572c6817acb80fa8c7977d
Sahil Chauksey <113386193+wazzer1803@users.noreply.github.com>
2024-03-04 20:19:49 +0530

    Update CustomTexts.jsx

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -6,7 +6,7 @@ import { textContainer, textVariant2} from '../utils/motion';
 
 
 export const TypingText = ({title,textStyles}) => (
-  <motion.p variants={textContainer} className={`font-normal text-[14px] text-black ${textStyles}`}>
+  <motion.p variants={textContainer} className={`font-normal text-[14px] text-gray-500 ${textStyles}`}>
 {
 Array.from(title).map((letter,index)=>{
  return(<motion.span variants={textVariant2} key={index}>
