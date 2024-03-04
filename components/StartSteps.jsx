15acc00e76b3e7259d43602c14e356fe7c6c6e06
Sahil Chauksey <113386193+wazzer1803@users.noreply.github.com>
2024-03-04 20:26:20 +0530

    Update StartSteps.jsx

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -2,10 +2,10 @@ import styles from '../styles'
 
 const StartSteps = ({key,number,text}) => (
   <div className={`${styles.flexCenter} flex-row`}>
-    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
- <p className="font-bold text-[20px] text-white">0{number}</p>
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-slate-600`}>
+ <p className="font-bold text-[20px] text-black">0{number}</p>
     </div>
-   <p className="flex-1 ml-[30px] font-extrabold text-[20px] text-white leading-[32px]">
+   <p className="flex-1 ml-[30px] font-extrabold text-[20px] text-gray-700 leading-[32px]">
     {text}
    </p>
   </div>
